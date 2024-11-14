b62361429eddaa42d8cf567db8fe271a366aafe4
Sahiramarte17 <sahiramarte17@gmail.com>
2024-11-14 11:14:13 -0500

    creating models bokk.ts

---
+++ client/src/models/Book.ts
@@ -0,0 +1,20 @@
+// src/models/Book.ts
+import mongoose, { Document, Schema } from 'mongoose';
+
+interface IBook extends Document {
+  title: string;
+  author: string;
+  genre: string;
+  description: string;
+}
+
+const BookSchema = new Schema<IBook>({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  genre: { type: String, required: true },
+  description: { type: String, required: true }
+});
+
+const Book = mongoose.model<IBook>('Book', BookSchema);
+
+export default Book;
