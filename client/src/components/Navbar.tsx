dfb3da065003601a60351dc91e13400b84c48b46
Sahiramarte17 <sahiramarte17@gmail.com>
2024-11-15 08:01:48 -0500

    Adding button to navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,9 +1,22 @@
+import React from 'react';
+
 const Navbar = () => {
-    return (
-      <header className='display-flex justify-space-between align-center p-2 mint-green'>
-        <h1>login</h1>
-      </header>
-    );
-  };
-  
-  export default Navbar;
+  return (
+    <header className="display-flex justify-space-between align-center p-2 mint-green">
+      <h1>Login</h1>
+      <div className="button-group">
+        {/* Login button */}
+        <button className="login-btn" onClick={() => alert('Login clicked')}>
+          Login
+        </button>
+        
+        {/* Books button */}
+        <button className="books-btn" onClick={() => alert('4 Books clicked')}>
+          4 Books
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
