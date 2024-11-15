7ab4ab0abdcf803b1eff9405a34427e4c8decdad
kol <kol3wang@gmail.com>
2024-11-14 21:37:57 -0500

    defined typescript interfaces in openLibrary.interface.ts file

---
+++ server/src/types/express/openLibrary.interface.ts
@@ -0,0 +1,19 @@
+// /server/src/types/express/openLibrary.interface.ts
+
+export interface OpenLibraryBook {
+    key: string;
+    title: string;
+    author_name?: string[];
+    cover_i?: number;
+  }
+  
+  export interface OpenLibraryApiResponse {
+    docs: OpenLibraryBook[];
+  }
+  
+  export interface OpenLibraryBookDetails {
+    title: string;
+    authors: { name: string }[];
+    publish_date: string;
+    number_of_pages: number;
+  }
