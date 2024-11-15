b95cdbf74d302bca200d197b5b2ee3b6dbfaaf8e
Sahiramarte17 <sahiramarte17@gmail.com>
2024-11-14 21:02:22 -0500

    updating homepage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,15 @@
-import { Outlet } from 'react-router-dom';
+// src/App.tsx
+import React from 'react';
+import './App.css';
+import HomePage from './pages/Homepage';  // Fix the import to match the component name
 
-
-
-function App() {
-
+const App: React.FC = () => {
   return (
-    <div>
-      <Navbar />
-      <main className="container container-fluid mt-5">
-        <Outlet />
-      </main>
+    <div className="App">
+      <HomePage />  {/* Use the HomePage component here */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
