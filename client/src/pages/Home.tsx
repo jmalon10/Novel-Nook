89adabe3a6944c80483501951b1534b4c6e2f814
Judymae Jolibois <judymaejolibois12388@gmail.com>
2024-11-18 13:47:45 -0500

    added flex container

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -13,18 +13,22 @@ const Home = () => {
     checkLogin(); // Call checkLogin() function to update loginCheck state
   }, [loginCheck]);
 
-    return (
-      <div className="bg-gradient-to-b from-purple-500 to-purple-900 min-h-screen flex items-center justify-center text-white">
+  return (
+    <div className="bg-gradient-to-b from-purple-500 to-purple-900 min-h-screen flex flex-col text-white">
       {loginCheck ? (
-        <h1 className="text-4xl font-bold">Welcome to the home page!</h1>
+        <div className="flex flex-1 items-center justify-center">
+          <h1 className="text-4xl font-bold">Welcome to the home page!</h1>
+        </div>
       ) : (
-        <div className="text-center">
-          <h1 className="text-4xl font-bold">You are not logged in!</h1>
-          <p className="text-3xl font-bold underline mt-4">Please login to continue</p>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold">You are not logged in!</h1>
+            <p className="text-3xl font-bold underline mt-4">Please login to continue</p>
+          </div>
         </div>
       )}
     </div>
-    );
-  };
-  
-  export default Home;
+  );
+};
+
+export default Home;
