0e5f5c204dd4b73194f08cc268a19bb1c16627ad
Judymae Jolibois <judymaejolibois12388@gmail.com>
2024-11-18 11:36:16 -0500

    added import to main.tsx, added underline to homepage to ensure tailwind was installed

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -39,6 +39,7 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { ApolloProvider, ApolloClient, InMemoryCache, HttpLink, ApolloLink } from '@apollo/client';
 import { onError } from '@apollo/client/link/error';
+import './index.css'
 
 import App from './App.tsx';
 import SearchBooks from './pages/SearchBooks.tsx';
