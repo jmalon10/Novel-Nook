957c300ef508f3e0ce79fd23bb6984c14f3663b6
Jackie Maloney <jmaloney11277@gmail.com>
2024-11-18 00:17:50 -0500

    fix port issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     host: true, // Ensures Vite binds to all hosts, necessary for deployment environments like Render
-    port: Number(process.env.PORT) || 3000 // Use Render's PORT environment variable or default to 3000
+    port: Number(process.env.PORT) || 3001 // Use Render's PORT environment variable or default to 3000
   },
   base: process.env.NODE_ENV === 'production' ? '/your-app-name/' : '/'
 });
