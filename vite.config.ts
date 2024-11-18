9f9f39de165bac12d8a8d06634cb6fc7e190eb70
Jackie Maloney <jmaloney11277@gmail.com>
2024-11-18 00:27:49 -0500

    comment out vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-// vite.config.js
-import { defineConfig } from 'vite';
+// // vite.config.js
+// import { defineConfig } from 'vite';
 
-export default defineConfig({
-  server: {
-    host: true, // Ensures Vite binds to all hosts, necessary for deployment environments like Render
-    port: Number(process.env.PORT) || 3001 // Use Render's PORT environment variable or default to 3000
-  },
-  base: process.env.NODE_ENV === 'production' ? '/your-app-name/' : '/'
-});
+// export default defineConfig({
+//   server: {
+//     host: true, // Ensures Vite binds to all hosts, necessary for deployment environments like Render
+//     port: Number(process.env.PORT) || 3001 // Use Render's PORT environment variable or default to 3000
+//   },
+//   base: process.env.NODE_ENV === 'production' ? '/your-app-name/' : '/'
+// });
